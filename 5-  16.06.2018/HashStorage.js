3e4bf565bfb9b3f8bbeaa6a04536a69f38061def
Anna Likhuta <34624873+AnnaLikhuta@users.noreply.github.com>
2018-06-28 06:44:00 +0300

    Add files via upload

--- 5-  16.06.2018/HashStorage.js
+++ 5-  16.06.2018/HashStorage.js
@@ -2,10 +2,9 @@ function HashStorage ( ){
    var self=this; 
    self.objInfo={};
    self.addValue = function (key,value){
-       //console.log(value);
-       self.objInfo[key]=value; // почему не срабатывал self.key=value
-       //console.log(self.objInfo)
-       }
+     self.objInfo[key]=value; // почему не срабатывал self.key=value
+         }
+
     self.getValue = function(key){
        if(key in self.objInfo)
        return self.objInfo[key];
@@ -15,11 +14,17 @@ function HashStorage ( ){
 
     }
     self.deleteValue = function(key){
+        if(!self.objInfo[key]){
+            return console.log('false. elem is not finded')
+        }
+        else
         delete self.objInfo[key];
+        return console.log('true. elem delete')
     }
     self.getKeys = function(){
-        for (var k in self.objInfo)
+        for (var k in self.objInfo){
          console.log( k);
+         console.log(self.objInfo[k]) ;}
     }
 
 }
@@ -51,22 +56,17 @@ function getInfo () {
         console.log('Коктель:  ' + nameCoctail)
         console.log(drink);
     }
-    /*if (!drinkStorage.getValue(nameCoctail)){
-        console.log('Информации нет по данному коктелю')
-        console.log('Для напитка ' + nameCoctail + drinkStorage.objInfo);
-    }    else{
-       
-    } */
 }
 
 function deleteCoctail (){
     var nameCoctail = prompt('Название коктеля, который желаете удалить');
     drinkStorage.deleteValue(nameCoctail);
-    console.log('Информация удалена');
+   
 
 }
 function allCoctail (){
     console.log('Имеются следующие напитки');
+    //console.log();
     drinkStorage.getKeys();
 
 }
