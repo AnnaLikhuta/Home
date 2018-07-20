2280bd64f6cd068f36d3a5f40c65edd64bf304f2
Anna Likhuta <34624873+AnnaLikhuta@users.noreply.github.com>
2018-07-20 22:30:36 +0300

    Add files via upload

--- 2-  26.05.2018/A2-palindrom.js
+++ 2-  26.05.2018/A2-palindrom.js
@@ -2,7 +2,7 @@
     var str=prompt('Ввести фразу для проверки');
     var smallStr=str.toLowerCase(); // к нижнему регистру
 
-    function editeWord (smallStr) { 
+    function editeWord (smallStr) {  //функция получает строку и выводит boolean
          var newStr='';
          for(var i=0; i<=smallStr.length-1; i++){
             if( (smallStr.charCodeAt(i)>=1072 && smallStr.charCodeAt(i)<=1103) ||   smallStr.charCodeAt(i)==1105 ) {  //проверка только на русские буквы
@@ -15,25 +15,21 @@
      if (newStr==''){
          newStr='не полиндром' // если возвращается пустая строка после проверки, 
      }                          // записываем  свой текст
-     return newStr;
-    }
-   
-    function checkWord(newStr){
-        var count=0;
-        for(i=0; i<=newStr.length-1; i++){
-            if ( (newStr[i]==newStr[newStr.length-1-i] )  || 
-             (newStr[i]=='е' &&  newStr[newStr.length-1-i]=='ё' ) ||
-             (newStr[i]=='ё' &&  newStr[newStr.length-1-i]=='е' )  ){
-                 continue;
-            }
-            else {
-                count++; }
-        }
-        return count;
-    }
+
+     var count=0;
+     for(i=0; i<=newStr.length-1; i++){
+         if ( (newStr[i]==newStr[newStr.length-1-i] )  || 
+          (newStr[i]=='е' &&  newStr[newStr.length-1-i]=='ё' ) ||
+          (newStr[i]=='ё' &&  newStr[newStr.length-1-i]=='е' )  ){
+              continue;
+         }
+         else {
+             return false; }
+     }
+     return true; }
 
   function printAnswer(count){
-    if (count==0){
+    if (count){
         console.log('You have palindrom');
     }
     else {
@@ -41,7 +37,6 @@
     }
   }
 
-    var newStr= editeWord(smallStr);
-   var count= checkWord(newStr);
+    var count= editeWord(smallStr);
    printAnswer(count);
 
--- 3-  02.06.2018/vowels.js
+++ 3-  02.06.2018/vowels.js
@@ -8,21 +8,22 @@
             countTry++;
         }
         return str;
-    }
+    } // отдельная функция для ввода
     
    
-   function checkWord(arr, word){
+   function checkWord( word){  // получает слово и выдает количество гласных
+    var vowelsArray=['а','о','у','э','я','и','е','ё','ю','ы'];
+
     var countVowels=0;
     for(var i=0; i<=word.length-1 ; i++){
-        if (arr.indexOf(word[i])!=-1){
+        if (vowelsArray.indexOf(word[i])!=-1){
           countVowels++;
         }
     }
     return countVowels;
    }
 
-   var vowelsArray=['а','о','у','э','я','и','е','ё','ю','ы'];
    var str=getWord();
-  var count= checkWord(vowelsArray, str );
+  var count= checkWord( str );
 
-    console.log('Количество гласных букв составляет: '+count);
+    console.log('Количество гласных букв составляет: '+count);  // вывод 
