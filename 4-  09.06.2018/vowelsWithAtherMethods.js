84b36e5b5fb79c9946251b4290091b64d7ab2d1d
Anna Likhuta <34624873+AnnaLikhuta@users.noreply.github.com>
2018-07-27 20:32:35 +0300

    Add files via upload

--- 4-  09.06.2018/vowelsWithAtherMethods.js
+++ 4-  09.06.2018/vowelsWithAtherMethods.js
@@ -8,32 +8,37 @@ function getStr(){
     return answer;
 }
 
-function allMethod (){
-   var strArray=getStr();
-   strArray=strArray.toLowerCase().split('');
- var countVowels=0;
- var vowels='аоуэяиеёюы'; //  для проверки
-//метод forEach
-   // console.log(strArray);
-    strArray.forEach( (item, i, arr) => {  
-       if(vowels.indexOf(item)!=-1)
-          countVowels++; 
-    });
-    console.log(countVowels + '-количество гласных букв по методу forEach')
-
-    //метод filter
-   var test= strArray.filter((item, i, arr ) => vowels.indexOf(item)!=-1 ).length; 
-   console.log(test+'- количество гласных по методу filter'); 
-
-     // метод  reduce
-  var test= strArray.reduce( (countVowels, item, i,arr)=> { 
-      if(vowels.indexOf(item)!=-1) 
-     countVowels++;
-     return countVowels;} , 0 );
-    console.log(test + '-количество гласных букв по методу reduce');
-
+function arrForEach(strArray){
+   
+    strArray=strArray.toLowerCase().split('');
+  var countVowels=0;
+  var vowels='аоуэяиеёюы'; //  для проверки
+      strArray.forEach( (item, i, arr) => {  
+        if(vowels.indexOf(item)!=-1)
+           countVowels++; 
+     });
+     return  countVowels;
 }
 
-allMethod ();
-        
+function arrFilter(strArray){
+    strArray=strArray.toLowerCase().split('');
+    var vowels='аоуэяиеёюы';
+    var test= strArray.filter((item, i, arr ) => vowels.indexOf(item)!=-1 ).length; 
+    return test;
+}
 
+function arrReduce(strArray){
+    strArray=strArray.toLowerCase().split('');
+    var vowels='аоуэяиеёюы';
+    var test= strArray.reduce( (countVowels, item, i,arr)=> { 
+        if(vowels.indexOf(item)!=-1) 
+       countVowels++;
+       return countVowels;} , 0 );
+       return test;
+}
+
+var strArray=getStr();
+//var countVowels=arrForEach(strArray);
+console.log(arrForEach(strArray) + '-количество гласных букв по методу forEach')
+console.log(arrFilter(strArray)+'- количество гласных по методу filter'); 
+console.log(arrReduce(strArray) + '-количество гласных букв по методу reduce');
