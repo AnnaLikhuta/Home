2d044bcdb494be4cae099f74def57d75ec2ef2ad
Anna Likhuta <34624873+AnnaLikhuta@users.noreply.github.com>
2018-08-08 20:58:14 +0300

    Add files via upload

--- dop/chainWord.js
+++ dop/chainWord.js
@@ -1,25 +1,21 @@
 ﻿
-function checkWord (where, arrWordWhat){
-    var check=0;
-    var abc=[]
-    var secondCheck=0;
+function checkWord (where, WordWhat){//where - где проверяем
+    WordWhat=WordWhat.split('');   // WordWhat- будем эти буквы перебирать для проверки
+    var check=0; // количество повторений
+    var abc=[]   // запоминать позиции букв. если 2 одинаковые буквы в слове
+    var secondCheck=0; // если такая буква была, то запомнить следующую позицию на которой она встречается
 
-
-
-
-
-    for(var i=0; i<=arrWordWhat.length-1; i++ ){
-        check=where.indexOf(arrWordWhat[i]);
+    for(var i=0; i<=WordWhat.length-1; i++ ){
+        check=where.indexOf(WordWhat[i]); 
         if(check!=-1  && abc.indexOf(check)==-1){
             abc.push(check)
         }
-        secondCheck=where.indexOf(arrWordWhat[i],check);
+        secondCheck=where.indexOf(WordWhat[i],check);
         if(secondCheck!=-1 && abc.indexOf(secondCheck)==-1){
             abc.push(secondCheck);
         }
 
     }
-
     if(abc.length>=3){
         return true;
     }
@@ -30,43 +26,45 @@ function checkWord (where, arrWordWhat){
 
 
 var dictionary=["ТАРА","ЛИПА","ТУРА","ЛУЖА","ПАРК","ЛОЖЬ","ЛУПА","ПЛОТ","МУРА","ПАУК","ПАУТ","ПЛУТ","ЛОЖА","СЛОТ","ПАРА"];
+
+
+
 /*
-var fstW='ЛИСА';
-var fshW='ЛОСЬ';
+
+var firstWord='ЛИСА';
+var finishWord='ЛОСЬ';
+
 */
-var fstW='МУХА';
-var fshW='СЛОН';
+
+
+var firstWord='МУХА';
+var finishWord='СЛОН';
+
 
 var n=0;
 var bool;
 var abc=[];
 
 //fstW=fstW.split('');
- //bool=checkWord (fshW, fstW);
-//console.log(bool);
+ bool=checkWord (finishWord, firstWord); // сразу проверить, что слова не подходят
 
-while(n<100){
-//fstW=fstW.split('');
+while(n<50){
     for(var i=0; i<=dictionary.length-1; i++){
-	
-
-        bool=checkWord (dictionary[i], fstW);
-	//console.log(bool);
-	//console.log(dictionary[i]);
-
+        bool=checkWord (dictionary[i], firstWord); // сравнение  dictionary[i] с  'ЛИСА'
         if(bool==true &&  abc.indexOf(dictionary[i])==-1){
-		abc.push(dictionary[i]);
-            console.log(dictionary[i]);
-            fstW=dictionary[i];
-	fstW=fstW.split('');
-	console.log(fstW)
-break;
-}
+		    abc.push(dictionary[i]); // слово, которое подошло добавить в массив, чтобы 2-ой раз не выбирать
+            console.log(firstWord);
+            firstWord=dictionary[i];
+            break;
+        }
         
-    //    else ;
-    
+}
+bool=checkWord (finishWord, firstWord) // проверка подходит ли итоговое
+if(bool==true){
+    firstWord=finishWord;
+    console.log(firstWord)
+    break;
 }
 n++;
 }
-console.log(fstW)
 console.log(abc)
