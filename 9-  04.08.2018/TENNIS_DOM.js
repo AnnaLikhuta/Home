857a515048dd60fa301b4f56b29734b231a11f68
Anna Likhuta <34624873+AnnaLikhuta@users.noreply.github.com>
2018-08-09 08:44:43 +0300

    Add files via upload

--- 9-  04.08.2018/TENNIS_DOM.js
+++ 9-  04.08.2018/TENNIS_DOM.js
@@ -1,20 +1,31 @@
  //описать константы
- 
+
+var gameFieldHeight=300;
+var gameFieldWidth=500;
+var marginElem=15;
+var bollSize=40;
+var RacketWidth= 10;
+var RacketHeight=80;
+
  
  var mainArea=document.createElement('div');
 
 var buttonStart=document.createElement('input');
 buttonStart.type='button'
 buttonStart.value='ПУСК';
-buttonStart.style.marginLeft='20px';
+buttonStart.style.marginLeft=marginElem+'px';
+buttonStart.onclick=start;
 mainArea.appendChild(buttonStart);
 
 
+ function abc() {
+     alert('dff')
+ }
 
 var countElem=document.createElement('div');
 countElem.style.display='inline-block';
-countElem.style.marginLeft='15px'
-countElem.style.width='400px';
+//countElem.style.marginLeft=marginElem;
+countElem.style.width='400px';  // другой способ отцентрировать  нужен
 countElem.style.textAlign='center';
 
 
@@ -35,29 +46,30 @@ mainArea.appendChild(countElem)
 
 var gameField=document.createElement('div');
 gameField.id='gameField';
-gameField.style.height='300px';
-gameField.style.width='500px';
+gameField.style.height=gameFieldHeight +'px';
+gameField.style.width=gameFieldWidth+'px';
 gameField.style.backgroundColor='aqua';
-gameField.style.margin='20px';
+gameField.style.margin=marginElem+'px';
 gameField.style.position='relative';
 mainArea.appendChild(gameField);
 
 var ball=document.createElement('div');
 ball.id='ball';
-ball.style.width='40px';
-ball.style.height='40px';
+ball.style.width=bollSize+'px';
+ball.style.height=bollSize+'px';
 ball.style.borderRadius='50%';
 ball.style.borderColor='black';
 ball.style.backgroundColor='magenta';
 ball.style.position='absolute';
-ball.style.left='50%'
-ball.style.bottom='50%'
+ball.style.left=gameFieldWidth/2-bollSize/2+'px';
+ball.style.bottom=gameFieldHeight/2-bollSize/2+'px';
 gameField.appendChild(ball);
+
 
 var leftRacket=document.createElement('div');
 leftRacket.id='leftRacket';
-leftRacket.style.width='10px';
-leftRacket.style.height='80px';
+leftRacket.style.width=RacketWidth+'px';
+leftRacket.style.height=RacketHeight+'px';
 leftRacket.style.borderColor='black';
 leftRacket.style.backgroundColor='yellow';
 leftRacket.style.position='absolute';
@@ -67,9 +79,8 @@ gameField.appendChild(leftRacket);
 
 var rightRacket=document.createElement('div');
 rightRacket.id='rightRacket';
-rightRacket.style.width='10px';
-rightRacket.style.height='80px';
-//rightRacket.style.border='1px solid black';
+rightRacket.style.width=RacketWidth+'px';
+rightRacket.style.height=RacketHeight+'px';
 rightRacket.style.backgroundColor='green';
 rightRacket.style.position='absolute';
 rightRacket.style.right='0%'
@@ -81,6 +92,63 @@ document.body.appendChild(mainArea)
 
 
 //движение мяча
+var ballH={
+    posX : gameFieldWidth/2-bollSize/2,
+    posY : gameFieldHeight/2-bollSize/2,
+    speedX : 8,
+    speedY : 2,
+    accelX : 0.5,
+    accelY :0.5,
+    width : bollSize,
+    height: bollSize,
+
+    update : function() {
+        var ballElem=document.getElementById('ball');
+        ballElem.style.left=Math.round(this.posX)+"px";
+        ballElem.style.top=Math.round(this.posY)+"px";
+    }
+}
+
+var areaH={
+    width : gameFieldWidth,
+    height : gameFieldHeight
+}
+
+function start() {
+    // плавное движение - от 25 кадр/сек, 1000мс/25к=40мс
+    setInterval(tick,40);
+}
+
+function tick() {
+
+    ballH.speedX+=ballH.accelX;
+    ballH.posX+=ballH.speedX;
+
+    // вылетел ли мяч правее стены?
+    if ( ballH.posX+ballH.width>areaH.width ) {
+        ballH.speedX=-ballH.speedX;
+        ballH.posX=areaH.width-ballH.width;
+    }
+    // вылетел ли мяч левее стены?
+    if ( ballH.posX<0 ) {
+        ballH.speedX=-ballH.speedX;
+        ballH.posX=0;
+    }
+
+    ballH.speedY+=ballH.accelY;
+    
+    ballH.posY+=ballH.speedY;
+
+    // вылетел ли мяч ниже пола?
+    if ( ballH.posY+ballH.height>areaH.height ) {
+        ballH.speedY=-ballH.speedY;
+        ballH.posY=areaH.height-ballH.height;
+    }
+
+    ballH.update();
+}
+
+ballH.update();
 
 
 
