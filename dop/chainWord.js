16cd3a6d9a07415eb334250372dfe70e5824e1f6
Anna Likhuta <34624873+AnnaLikhuta@users.noreply.github.com>
2018-08-13 23:28:03 +0300

    Add files via upload

--- dop/chainWord.js
+++ dop/chainWord.js
@@ -1,4 +1,12 @@
-﻿
+﻿// Если оставить ЛИСА-ЛОСЬ -> 
+// получаю (9) ["ЛИСА", "ЛИПА", "ЛУПА", "ПАУК", "ПАУТ", "ПЛУТ", "ПЛОТ", "СЛОТ", "ЛОСЬ"]
+// если МУХА-СЛОН -> 
+// получаю ["МУХА", "МУРА", "ТУРА", "ПАУТ", "ПЛУТ", "ЛУПА", "ПАУК", "ПАРК", "ПАРА", "ПАРА"
+// почему  не берет слово  ТАРА дальше? вот этот момент не могу понять
+// буду менять алгоритм перебора.Т,к. задание не сделано
+
+
+
 function checkWord (where, WordWhat){//where - где проверяем
     WordWhat=WordWhat.split('');   // WordWhat- будем эти буквы перебирать для проверки
     var check=0; // количество повторений
@@ -10,7 +18,8 @@ function checkWord (where, WordWhat){//where - где проверяем
         if(check!=-1  && abc.indexOf(check)==-1){
             abc.push(check)
         }
-        secondCheck=where.indexOf(WordWhat[i],check);
+        secondCheck=where.indexOf(WordWhat[i],check); // если к примеру проверяю каждую букву слова МАМА в слове МАЛО
+                                                        // и две буквы М с одинаковым номером позиции не будут учтены 
         if(secondCheck!=-1 && abc.indexOf(secondCheck)==-1){
             abc.push(secondCheck);
         }
@@ -27,44 +36,40 @@ function checkWord (where, WordWhat){//where - где проверяем
 
 var dictionary=["ТАРА","ЛИПА","ТУРА","ЛУЖА","ПАРК","ЛОЖЬ","ЛУПА","ПЛОТ","МУРА","ПАУК","ПАУТ","ПЛУТ","ЛОЖА","СЛОТ","ПАРА"];
 
+ // var firstWord='ЛИСА';
+   // var finishWord='ЛОСЬ';
 
-
-/*
-
-var firstWord='ЛИСА';
-var finishWord='ЛОСЬ';
-
-*/
-
-
-var firstWord='МУХА';
-var finishWord='СЛОН';
+    var firstWord='МУХА';
+   var finishWord='СЛОН';
 
 
 var n=0;
 var bool;
 var abc=[];
 
-//fstW=fstW.split('');
  bool=checkWord (finishWord, firstWord); // сразу проверить, что слова не подходят
-
-while(n<50){
+if(bool==false){
+    abc.push(firstWord); // добавляю в цепочку. с него начинаем искать
+}
+while(n<50){  // не доработана функция. поэтому страхуюсь конечным условием
     for(var i=0; i<=dictionary.length-1; i++){
         bool=checkWord (dictionary[i], firstWord); // сравнение  dictionary[i] с  'ЛИСА'
+        
         if(bool==true &&  abc.indexOf(dictionary[i])==-1){
 		    abc.push(dictionary[i]); // слово, которое подошло добавить в массив, чтобы 2-ой раз не выбирать
-            console.log(firstWord);
             firstWord=dictionary[i];
-            break;
+
+            //break;
         }
-        
+        bool=checkWord (finishWord, firstWord) // проверка подходит ли итоговое
+        if(bool==true){
+            firstWord=finishWord;
+            break;
+        }     
 }
-bool=checkWord (finishWord, firstWord) // проверка подходит ли итоговое
-if(bool==true){
-    firstWord=finishWord;
-    console.log(firstWord)
-    break;
-}
+
 n++;
 }
-console.log(abc)
+abc.push(firstWord); 
+
+console.log(abc)// итоговая цепочка
