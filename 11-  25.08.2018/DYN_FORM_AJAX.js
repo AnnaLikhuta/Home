a8c544effb0f91ca2c92cd9633765a123a0577bf
Anna Likhuta <34624873+AnnaLikhuta@users.noreply.github.com>
2018-08-28 01:26:38 +0300

    Add files via upload

---
+++ 11-  25.08.2018/DYN_FORM_AJAX.js
@@ -0,0 +1,285 @@
+
+var  buttElem=document.getElementById('buttElem');
+window.onload=restoreInfo;
+var aa;
+var bb;
+
+//формы длязагрузки
+var formDef1=
+[
+  {label:'Название сайта:',kind:'longtext',name:'sitename'},
+  {label:'URL сайта:',kind:'longtext',name:'siteurl'},
+  {label:'Посетителей в сутки:',kind:'number',name:'visitors'},
+  {label:'E-mail для связи:',kind:'shorttext',name:'email'},
+  {label:'Рубрика каталога:',kind:'combo',name:'division',
+    variants:[{text:'здоровье',value:1},{text:'домашний уют',value:2},{text:'бытовая техника',value:3}]},
+  {label:'Размещение:',kind:'radio',name:'payment',
+    variants:[{text:'бесплатное',value:1},{text:'платное',value:2},{text:'VIP',value:3}]},
+  {label:'Разрешить отзывы:',kind:'check',name:'votes'},
+  {label:'Описание сайта:',kind:'memo',name:'description'},
+  {label:'Опубликовать:',kind:'submit'},
+];
+
+var formDef2=
+[
+  {label:'Фамилия:',kind:'longtext',name:'lastname'},
+  {label:'Имя:',kind:'longtext',name:'firstname'},
+  {label:'Отчество:',kind:'longtext',name:'secondname'},
+  {label:'Возраст:',kind:'number',name:'age'},
+  {label:'Зарегистрироваться:',kind:'submit'},
+];
+
+
+
+
+
+var ajaxHandlerScript="https://fe.it-academy.by/AjaxStringStorage2.php";
+var updatePassword;
+var stringName='LIKHUTA_DYN_FORM_AJAX';
+
+// заись на сохранение. на кнопке
+function storeInfo() {
+    updatePassword=Math.random();
+    $.ajax( {
+            url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
+            data : { f : 'LOCKGET', n : stringName, p : updatePassword },
+            success : lockGetReady, error : errorHandler
+        }
+    );
+}
+
+function lockGetReady(callresult) {
+    if ( callresult.error!=undefined )
+        alert(callresult.error); 
+    else {
+        // нам всё равно, что было прочитано - 
+        // всё равно перезаписываем
+        var info={
+            arr1 : formDef1,
+            arr2 : formDef2
+        };
+        $.ajax( {
+                url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
+                data : { f : 'UPDATE', n : stringName, v : JSON.stringify(info), p : updatePassword },
+                success : updateReady, error : errorHandler
+            }
+        );
+    }
+}
+
+function updateReady(callresult) {
+    if ( callresult.error!=undefined )
+        alert(callresult.error); 
+}
+
+function restoreInfo() {
+    $.ajax(
+        {
+            url : ajaxHandlerScript, type : 'POST', cache : false, dataType:'json',
+            data : { f : 'READ', n : stringName },
+            success : readReady, error : errorHandler
+        }
+    );
+}
+
+function readReady(callresult) {
+    if ( callresult.error!=undefined )
+        alert(callresult.error); 
+    else if ( callresult.result!="" ) {
+        var info=JSON.parse(callresult.result); 
+          aa=info.arr1;
+          bb=info.arr2;
+    }
+}
+
+function errorHandler(jqXHR,statusStr,errorStr) {
+    alert(statusStr+' '+errorStr);
+}
+// сохранить  данные
+storeInfo();
+
+// запуск конструктора формы
+buttElem.onclick=function(){
+  createForm(aa);
+  createForm(bb);
+  
+}
+
+var i=0; // глобальный счетчик для формы
+function createForm (forForm){  
+
+function createFormBasic (){ //создать форму основу
+
+  var elemForm= document.createElement('form');
+        var nameForm='nameForm'+i+'';
+         elemForm.name=nameForm;
+         document.body.appendChild(elemForm);
+         i++; 
+         return nameForm;
+}
+var nameForm =createFormBasic();
+
+function getFormElem(obj){ // перебираем элементы  вызываем нужную функцию
+  var objLabel;
+  var objName;
+  var objVariant;
+for (var i=0; i<=obj.length-1; i++){
+objLabel= obj[i].label;
+objName = obj[i].name;
+
+switch (obj[i].kind){
+  case 'longtext':{
+     forLongtextElem (objLabel, objName);
+    break;
+  }
+  case 'number':{
+    forNumberElem(objLabel, objName);
+    break;
+  }
+  case 'shorttext':{
+    forEmailElem(objLabel, objName);
+    break;
+
+  }
+  case 'combo':{
+    objVariant = obj[i].variants; 
+  forComboElem(objLabel, objVariant);
+    break;
+  }
+  case 'radio':{
+    objVariant = obj[i].variants;
+    forRadioElem(objLabel, objVariant);
+    break;
+  }
+  case 'check':{
+    forCheckElem(objLabel, objName);
+    break;
+  }
+  case 'memo':{
+    forMemoElem(objLabel, objName);
+    break;
+
+  }
+  case 'submit' :{
+    forSubmit(objLabel);
+    break;
+  }
+  
+  
+  default: break;
+}
+
+}
+
+}
+
+
+
+
+
+function abc(label){ // создать label, input
+  var elemInput = document.createElement('input');
+  var elemLabel = document.createElement('label');
+  elemLabel.appendChild( document.createTextNode(' '+ label+'' ));  // здесь для текста доcтуп
+  var elem = document.createElement('p');
+  elem.appendChild(elemLabel).appendChild(elemInput);
+  document[nameForm].appendChild(elem);
+  return elemInput;
+
+}
+ function forLongtextElem (label, name){
+  var elemInput = abc(label);
+    elemInput.name=name;
+    elemInput.type='text';
+
+ }
+
+ function forNumberElem(label, name){
+  var elemInput = abc(label);
+  elemInput.name=name;
+  elemInput.type='number';
+  elemInput.min='0';
+  }
+  function forEmailElem(label, name){ 
+    var elemInput = abc(label);
+    elemInput.name=name;
+    elemInput.type='email';
+
+  }
+
+
+ function forComboElem (label, arrVar){
+   
+   //console.log(arrVar);
+   var selectElem = document.createElement('select');
+    var labelElem = document.createElement('label');
+    var optionElem;
+    labelElem.appendChild(document.createTextNode(label));
+    for(var i=0; i<=arrVar.length-1; i++){  
+      
+     optionElem = document.createElement('option');
+     optionElem.value=i+1;
+    
+    optionElem.appendChild(document.createTextNode(arrVar[i].text));
+    labelElem.appendChild(selectElem).appendChild(optionElem);
+    
+    }
+     document[nameForm].appendChild(labelElem);
+
+   // document.getElementById('elemForm').appendChild(labelElem);
+
+ }
+
+ function forRadioElem(label, arrVar){
+  var elemInput;
+  var spanElem;
+  var elemLabel = document.createElement('label');
+  elemLabel.appendChild( document.createTextNode(label));  // здесь для текста доcтуп
+  
+  for( var i=0; i<=arrVar.length-1; i++){
+    elemInput = document.createElement('input')
+    elemInput.type='radio';
+    elemInput.name='abc';
+    elemInput.value=i+1;
+    spanElem = document.createElement('span');
+  spanElem.appendChild(document.createTextNode(arrVar[i].text));
+  
+  elemLabel.appendChild(elemInput);
+  elemLabel.appendChild(spanElem);
+  }
+  var elem = document.createElement('p');
+  elem.appendChild(elemLabel);
+  document[nameForm].appendChild(elem);
+
+  }
+
+  function forCheckElem(label, name){
+    var elemInput = abc(label);
+    elemInput.name=name;
+    elemInput.type='checkbox';
+
+  }
+
+  function forMemoElem(label, name){
+    var elemInput = document.createElement('textarea');
+    var elemLabel = document.createElement('label');
+    elemLabel.appendChild( document.createTextNode(label));  // здесь для текста доcтуп
+    var elem = document.createElement('p');
+    elem.appendChild(elemLabel).appendChild(elemInput);
+    document[nameForm].appendChild(elem);
+    elemInput.name=name;
+    
+
+  }
+
+  function forSubmit (label){
+    var elemInput = abc(label);
+    elemInput.type='submit';
+   
+
+  }
+ 
+  getFormElem(forForm);
+
+
+}
